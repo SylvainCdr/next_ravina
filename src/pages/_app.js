31e99bf424761d7185cad44faff12450bd331969
sly <sylvaincadoret@hotmail.com>
2026-08-26 15:11:35 +0200

    feat: integrate Google Analytics for improved tracking of page view

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,37 +3,51 @@ import "@/index.css";
 // import { AppProvider } from "@/Components/appContext";
 import Template from "@/Components/Template/Template";
 import Head from "next/head";
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
+const GA_MEASUREMENT_ID = "G-NS69E5HFSP";
 
- function App({ Component, pageProps }) {
-
+function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-  
+      if (typeof window.gtag === "function") {
+        window.gtag("config", GA_MEASUREMENT_ID, { page_path: url });
+      }
     };
-    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
-  
+
   return (
     <>
       <Head>
         {/* Viewport Meta Tag for Mobile Optimization */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-   
-      
-          <Template>
-            <Component {...pageProps} />
-          </Template>
-  
-     
+
+      {/* Google Analytics (GA4) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+
+      <Template>
+        <Component {...pageProps} />
+      </Template>
     </>
   );
 }
